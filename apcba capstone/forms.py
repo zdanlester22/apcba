a2502cf09559df475ee9c03fc61d9cfef9c55aad
zdanlester22 <dare.zapatero.au@phinmaed.com>
2024-01-29 00:44:23 +0800

    first commit

---
+++ apcba capstone/forms.py
@@ -0,0 +1,232 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, SelectField, BooleanField
+from wtforms.validators import DataRequired,Optional,Email
+from flask_wtf.file import FileField, FileAllowed
+from wtforms.fields import StringField, SubmitField, IntegerField, DateField, TextAreaField
+from wtforms.validators import DataRequired, NumberRange, Length, InputRequired
+from wtforms.fields import FloatField, TimeField
+
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    contact = StringField('Contact', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    role = SelectField('Role', choices=[('student', 'Student'), ('teacher', 'Teacher'), ('admin', 'Admin')],
+                       validators=[DataRequired()])
+    
+
+class UpdateUserForm(FlaskForm):
+    new_username = StringField('New Username')
+    new_name = StringField('New Name', validators=[DataRequired()])
+    new_contact = StringField('New Contact', validators=[DataRequired()])
+    new_password = PasswordField('New Password')
+    new_role = SelectField('New Role', choices=[('admin', 'Admin'), ('teacher', 'Teacher'), ('student', 'Student')])
+    submit = SubmitField('Update User')
+
+    
+
+class AnnouncementForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post Announcement')
+
+class CertificateForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    pdf = FileField('PDF File', validators=[FileAllowed(['pdf'], 'PDFs only!')])
+    submit = SubmitField('Upload Certificate')
+
+
+class UserAccountForm(FlaskForm):
+   # Program Information
+    course = StringField('Course')
+    year_level = StringField('Year Level')
+    lrn = StringField("DedEd Learner's Reference Number (LRN)")
+    curriculum_year = StringField('Curriculum Year')
+    
+    # Personal Data
+    last_name = StringField('Last Name', validators=[DataRequired()])
+    first_name = StringField('First Name', validators=[DataRequired()])
+    middle_name = StringField('Middle Name', validators=[DataRequired()])
+    
+    # Place of Birth
+    place_of_birth = StringField('Place of Birth', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    gender = SelectField('Gender', choices=[('male', 'Male'), ('female', 'Female')], validators=[DataRequired()])
+    civil_status = StringField('Civil Status', validators=[DataRequired()])
+    nationality = StringField('Nationality', validators=[DataRequired()])
+    
+    # Current Address
+    current_country = StringField('Country', validators=[DataRequired()])
+    current_region = StringField('Region', validators=[DataRequired()])
+    current_province = StringField('Province', validators=[DataRequired()])
+    current_municipality = StringField('Municipality', validators=[DataRequired()])
+    current_complete_address = TextAreaField('Complete Address (Rm# Bldg./House#, Street, Brgy.)')
+    
+    # Permanent Address
+    permanent_country = StringField('Country', validators=[DataRequired()])
+    permanent_region = StringField('Region', validators=[DataRequired()])
+    permanent_province = StringField('Province', validators=[DataRequired()])
+    permanent_municipality = StringField('Municipality', validators=[DataRequired()])
+    permanent_complete_address = TextAreaField('Complete Address (Rm# Bldg./House#, Street, Brgy.)')
+    
+    # Contact Information
+    school_issued_mobile = StringField('School Issued Mobile No.', validators=[DataRequired()])
+    school_email = StringField('School Email', validators=[DataRequired(), Email()])
+    
+    personal_country_code = StringField('Country Code', validators=[DataRequired()])
+    personal_tel_no = StringField('Tel No.')
+    personal_mobile_1 = StringField('Personal Mobile No. 1', validators=[DataRequired()])
+    personal_mobile_2 = StringField('Personal Mobile No. 2')
+    personal_mobile_3 = StringField('Personal Mobile No. 3')
+    personal_email = StringField('Personal Email', validators=[DataRequired(), Email()])
+    
+    # Religion and Social Media
+    religion = StringField('Religion', validators=[DataRequired()])
+    facebook = StringField('Facebook')
+    twitter = StringField('Twitter')
+    
+    # Educational Data
+    school_level = StringField('School Level', validators=[DataRequired()])
+    school_name = StringField('Name of School', validators=[DataRequired()])
+    course = StringField('Course', validators=[DataRequired()])
+    year_graduated = StringField('Year Graduated')
+    last_school_attended = StringField('Last School Attended', validators=[DataRequired()])
+    
+    # Economic Data
+    father_last_name = StringField("Father's Last Name", validators=[DataRequired()])
+    father_first_name = StringField("Father's First Name", validators=[DataRequired()])
+    father_middle_name = StringField("Father's Middle Name", validators=[DataRequired()])
+    father_occupation = StringField("Father's Occupation", validators=[DataRequired()])
+    father_contact_no = StringField("Father's Contact No.", validators=[DataRequired()])
+    
+    mother_last_name = StringField("Mother's Last Name", validators=[DataRequired()])
+    mother_first_name = StringField("Mother's First Name", validators=[DataRequired()])
+    mother_middle_name = StringField("Mother's Middle Name", validators=[DataRequired()])
+    mother_occupation = StringField("Mother's Occupation", validators=[DataRequired()])
+    mother_contact_no = StringField("Mother's Contact No.", validators=[DataRequired()])
+    
+    num_brothers = IntegerField('No. of Brothers', validators=[DataRequired()])
+    num_sisters = IntegerField('No. of Sisters', validators=[DataRequired()])
+    
+    guardian_occupation = StringField('Guardian Occupation', validators=[DataRequired()])
+    guardian_relationship = StringField('Guardian Relationship', validators=[DataRequired()])
+    guardian_contact_no = StringField('Guardian Contact No.', validators=[DataRequired()])
+    
+    # Emergency Contact
+    emergency_contact_name = StringField('Emergency Contact Name', validators=[DataRequired()])
+    emergency_contact_relationship = StringField('Emergency Contact Relationship', validators=[DataRequired()])
+    emergency_contact_no = StringField('Emergency Contact No.', validators=[DataRequired()])
+    emergency_contact_address = StringField('Emergency Contact Address', validators=[DataRequired()])
+
+class CourseForm(FlaskForm):
+    abbreviation = StringField('Abbreviation', validators=[DataRequired()])
+    title = StringField('Title Name', validators=[DataRequired()])
+    school_year = StringField('School Year', validators=[DataRequired()])
+    year = StringField('Year', validators=[DataRequired()])
+    Class = StringField('Class', validators=[DataRequired()])
+    semesters = SelectField('Semesters', choices=[('1st Semester', '1st Semester'), ('2nd Semester', '2nd Semester')], validators=[DataRequired()])
+    course_type = SelectField('Type', choices=[('SHS', 'SHS'), ('College', 'College')], validators=[DataRequired()])
+    is_active = BooleanField('Active', default=True)
+    submit = SubmitField('Create Course')
+
+class SubjectForm(FlaskForm):
+    abbreviation = StringField('Abbreviation', validators=[DataRequired()])
+    title = StringField('Title', validators=[DataRequired()])
+    unit = IntegerField('Unit', validators=[DataRequired()])
+    course_id = SelectField('Course', coerce=int, validators=[DataRequired()])
+    def set_course_choices(self, courses):
+        self.course_id.choices = [(course.id, course.title) for course in courses]
+
+
+    
+class FilterForm(FlaskForm):
+    school_year = StringField('School Year')
+    semester = IntegerField('Semester')
+    course_type = SelectField('Course Type', choices=[('', 'All'), ('shs', 'SHS'), ('college', 'College')])
+
+class SectionForm(FlaskForm):
+    name = StringField('Section Name', validators=[DataRequired()])
+    capacity = IntegerField('Capacity', validators=[DataRequired()])
+    course_id = SelectField('Course', coerce=int, validators=[DataRequired()])
+    teacher_id = SelectField('Teacher', coerce=int, validators=[DataRequired()])
+
+    def set_teacher_choices(self, teachers):
+        self.teacher_id.choices = [(teacher.teacher_id, teacher.name) for teacher in teachers]
+
+
+class TeacherForm(FlaskForm):
+    teacher_id = IntegerField('Teacher ID', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Add Teacher')
+
+class StudentForm(FlaskForm):
+    student_id = StringField('Student ID', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField('Add Student')
+
+class UpdateStudentForm(FlaskForm):
+    student_id = StringField('Student ID', render_kw={'readonly': True})  # Read-only field for student_id
+    updated_name = StringField('Updated Name', validators=[DataRequired()])
+    submit = SubmitField('Update Student')
+
+class ModuleForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    year = IntegerField('Year', validators=[DataRequired()])
+    course_id = SelectField('Course', coerce=int, validators=[DataRequired()])
+    pdf_file = FileField('PDF File', validators=[DataRequired()])
+
+class EnrollmentForm(FlaskForm):
+    student_id = SelectField('Student ID', validators=[DataRequired()])
+    course_id = SelectField('Select Course', coerce=int)
+    section_id = SelectField('Select Section', coerce=int)
+    is_approved = BooleanField('Is Approved')
+    submit = SubmitField('Enroll')
+
+class EnrolliesForm(FlaskForm):
+    name = StringField('Full Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    level = SelectField('Choose your level', choices=[('college', 'College'), ('senior_high', 'Senior High School')],
+                        validators=[DataRequired()])
+    address = StringField('Home Address', validators=[DataRequired()])
+    contact_number = StringField('Contact Number', validators=[DataRequired()])
+    date_of_birth = DateField('Date of Birth', validators=[DataRequired()])
+    place_of_birth = StringField('Place of Birth', validators=[DataRequired()])
+    gender = StringField('Gender', validators=[DataRequired()])
+    nationality = StringField('Nationality', validators=[DataRequired()])
+    religion = StringField('Religion', validators=[DataRequired()])
+    previous_school_info = StringField('Previous School Information')
+    grade_last_completed = StringField('Grade/Class Last Completed')
+    academic_achievements = TextAreaField('Academic Achievements')
+    proof_of_address = StringField('Proof of Address')
+    parent_names = StringField('Names of Parents/Guardians', validators=[DataRequired()])
+    parent_contact_info = StringField('Contact Information of Parents/Guardians', validators=[DataRequired()])
+    parent_occupation = StringField('Occupation of Parents/Guardians', validators=[DataRequired()])
+    medical_history = TextAreaField('Medical History', validators=[DataRequired()])
+    immunization_records = TextAreaField('Immunization Records', validators=[DataRequired()])
+    medical_certificate = FileField('Medical Certificate', validators=[DataRequired()])
+    photos = FileField('Recent ID-size Photos')
+    form_138 = FileField('Form 138 (Report Card) or Permanent Record')
+    moral_character_certificate = FileField('Good Moral Character Certificate')
+    special_needs = TextAreaField('Special Needs or Accommodations')
+
+class GradeForm(FlaskForm):
+    period_1 = StringField('Period 1', validators=[InputRequired()])
+    period_2 = StringField('Period 2', validators=[InputRequired()])
+    period_3 = StringField('Period 3', validators=[InputRequired()])
+    final_grade = FloatField('Final Grade', render_kw={'readonly': True})
+    is_passed = StringField('Status', render_kw={'readonly': True})
+
+    submit = SubmitField('Submit')
+
+class ScheduleForm(FlaskForm):
+    day_of_week = StringField('Day of Week', validators=[InputRequired()])
+    start_time = TimeField('Start Time', validators=[InputRequired()])
+    end_time = TimeField('End Time', validators=[InputRequired()])
+    submit = SubmitField('Add Schedule')
---
+++ apcba capstone/models.py
@@ -0,0 +1,235 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    contact = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    role = db.Column(db.String(10), nullable=False)
+
+    # Establishing a one-to-one relationship with user_account
+    user_account = db.relationship('user_account', backref='user', uselist=False, lazy=True)
+
+
+class user_account(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(255))
+    age = db.Column(db.Integer)
+    birthday = db.Column(db.Integer)  # Assuming birthday is an IntegerField
+    place_of_birth = db.Column(db.String(255))
+    gender = db.Column(db.String(10))
+    civil_status = db.Column(db.String(50))
+    nationality = db.Column(db.String(50))
+    current_country = db.Column(db.String(100))
+    current_region = db.Column(db.String(100))
+    current_province = db.Column(db.String(100))
+    current_municipality = db.Column(db.String(100))
+    current_complete_address = db.Column(db.Text)
+    permanent_country = db.Column(db.String(100))
+    permanent_region = db.Column(db.String(100))
+    permanent_province = db.Column(db.String(100))
+    permanent_municipality = db.Column(db.String(100))
+    permanent_complete_address = db.Column(db.Text)
+    school_issued_mobile = db.Column(db.String(20))
+    school_email = db.Column(db.String(120))
+    personal_country_code = db.Column(db.String(10))
+    personal_tel_no = db.Column(db.String(20))
+    personal_mobile_1 = db.Column(db.String(20))
+    personal_mobile_2 = db.Column(db.String(20))
+    personal_mobile_3 = db.Column(db.String(20))
+    personal_email = db.Column(db.String(120))
+    religion = db.Column(db.String(50))
+    facebook = db.Column(db.String(255))
+    twitter = db.Column(db.String(255))
+    school_level = db.Column(db.String(100))
+    school_name = db.Column(db.String(255))
+    course = db.Column(db.String(100))
+    year_graduated = db.Column(db.String(50))
+    last_school_attended = db.Column(db.String(255))
+    father_last_name = db.Column(db.String(255))
+    father_first_name = db.Column(db.String(255))
+    father_middle_name = db.Column(db.String(255))
+    father_occupation = db.Column(db.String(255))
+    father_contact_no = db.Column(db.String(20))
+    mother_last_name = db.Column(db.String(255))
+    mother_first_name = db.Column(db.String(255))
+    mother_middle_name = db.Column(db.String(255))
+    mother_occupation = db.Column(db.String(255))
+    mother_contact_no = db.Column(db.String(20))
+    num_brothers = db.Column(db.Integer)
+    num_sisters = db.Column(db.Integer)
+    guardian_occupation = db.Column(db.String(255))
+    guardian_relationship = db.Column(db.String(50))
+    guardian_contact_no = db.Column(db.String(20))
+    emergency_contact_name = db.Column(db.String(255))
+    emergency_contact_relationship = db.Column(db.String(50))
+    emergency_contact_no = db.Column(db.String(20))
+    emergency_contact_address = db.Column(db.String(255))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), unique=True, nullable=False)
+
+    
+
+
+class Announcement(db.Model):
+    __tablename__ = 'announcement'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp())
+    author_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    author = db.relationship('User', backref='announcements')
+
+class Certificate(db.Model):
+    __tablename__ = 'certificate'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    pdf = db.Column(db.String(100), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
+
+class Section(db.Model):
+    __tablename__ = 'sections'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    course = db.relationship('Course', back_populates='sections', lazy=True)
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'))  # Correct the foreign key reference
+    teacher = db.relationship('Teacher', back_populates='section', uselist=False)
+    subjects = db.relationship('Subject', back_populates='section', lazy=True)
+
+
+class Subject(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    abbreviation = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    unit = db.Column(db.Integer)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    course = db.relationship('Course', back_populates='subjects', lazy=True)
+    section_id = db.Column(db.Integer, db.ForeignKey('sections.id'))
+    section = db.relationship('Section', back_populates='subjects')
+    grades = db.relationship('Grades', back_populates='subject', lazy=True)
+    schedules = db.relationship('Schedule', back_populates='subject', lazy=True)
+   
+
+class Teacher(db.Model):
+    __tablename__ = 'teacher'
+    id = db.Column(db.Integer, primary_key=True)
+    teacher_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    section = db.relationship('Section', back_populates='teacher', uselist=False)  # Using 'section' instead of 'sections'
+    
+
+    
+  
+class Module(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    course = db.relationship('Course', backref='modules')
+    pdf_filename = db.Column(db.String(255), nullable=False)
+
+class Enrollment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    course_id = db.Column(db.Integer, db.ForeignKey('course.id'), nullable=False)
+    section_id = db.Column(db.Integer, db.ForeignKey('sections.id'), nullable=False)
+    is_approved = db.Column(db.Boolean, default=False)
+
+    enrolled_student_rel = db.relationship('Student', backref='enrollment', single_parent=True)
+    enrolled_course_rel = db.relationship('Course', backref='enrollments_course')
+    enrolled_section_rel = db.relationship('Section', backref='enrollments_section')
+
+
+
+class Enrollies(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    level = db.Column(db.String(20), nullable=False)
+    address = db.Column(db.String(255))
+    contact_number = db.Column(db.String(15))
+    date_of_birth = db.Column(db.Date)
+    place_of_birth = db.Column(db.String(100))
+    gender = db.Column(db.String(10))
+    nationality = db.Column(db.String(50))
+    religion = db.Column(db.String(50))
+    previous_school_info = db.Column(db.String(100))
+    grade_last_completed = db.Column(db.String(10))
+    academic_achievements = db.Column(db.String(255))
+    proof_of_address = db.Column(db.String(255))
+    parent_names = db.Column(db.String(200))
+    parent_contact_info = db.Column(db.String(30))
+    parent_occupation = db.Column(db.String(100))
+    medical_history = db.Column(db.String(255))
+    immunization_records = db.Column(db.String(255))
+    medical_certificate = db.Column(db.String(255))
+    photos = db.Column(db.String(255))
+    form_138 = db.Column(db.String(255))
+    moral_character_certificate = db.Column(db.String(255))
+    special_needs = db.Column(db.String(255))
+
+    
+
+class Student(db.Model):
+    __tablename__ = 'student'
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    grades = db.relationship('Grades', back_populates='student', lazy=True)
+
+    enrollments = db.relationship('Enrollment', backref='student', lazy=True)
+
+
+
+class Course(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    course_type = db.Column(db.String(20), nullable=False)
+    abbreviation = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    Class = db.Column(db.String(100))
+    year = db.Column(db.String(20), nullable=False)
+    school_year = db.Column(db.String(20), nullable=False)
+    semesters = db.Column(db.String(20), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+    sections = db.relationship('Section', back_populates='course', lazy=True)
+    subjects = db.relationship('Subject', back_populates='course', lazy=True)
+
+
+class Grades(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    period_1 = db.Column(db.String(5))
+    period_2 = db.Column(db.String(5))
+    period_3 = db.Column(db.String(5))
+    final_grade = db.Column(db.String(5))
+    is_passed = db.Column(db.Boolean, default=False)
+    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
+
+    student = db.relationship('Student', back_populates='grades')
+    subject = db.relationship('Subject', back_populates='grades')
+
+
+class Schedule(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    day_of_week = db.Column(db.String(20), nullable=False)
+    start_time = db.Column(db.Time, nullable=False)
+    end_time = db.Column(db.Time, nullable=False)
+    subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
+    subject = db.relationship('Subject', back_populates='schedules', lazy=True)
+
+
+
