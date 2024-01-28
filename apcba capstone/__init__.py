7734cbcbbf4f70f7bc2aa546a8311a2b378e94e1
zdanlester22 <dare.zapatero.au@phinmaed.com>
2024-01-29 01:43:13 +0800

    commit

--- apcba capstone/__init__.py
+++ apcba capstone/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from app import app
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -15,8 +16,12 @@ def create_app():
     login_manager.init_app(app)
     login_manager.login_view = 'login'
 
-    from your_package import models  # Import your models here
-    from your_package import app as app_blueprint
-    app.register_blueprint(app_blueprint)
+    from .models import db
+
+
+    app.register_blueprint(app)
+
+    with app.app_context():
+        db.create_all()
 
     return app
